cb0bf3bea1c983e17a6489c22d067181259bf9f3
Fuyad <fuyad56@gmail.com>
2022-11-04 16:47:16 +0600

    Second commit

---
+++ tamplate.js
@@ -0,0 +1,18 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName + " " + lastName + " is a good boy.";
+console.log(fullName);
+
+const fullName2 = `${firstName} ${lastName} is a good boy.`;
+console.log(fullName2);
+
+const multiLine = " I love you\n"
+    + " i miss you\n"
+    + "I need you";
+    console.log(multiLine);
+
+const multiLine2 = `I love you
+I miss you
+No. i don't need you
+Baily road e dorkar nai.`
+console.log(multiLine2);
