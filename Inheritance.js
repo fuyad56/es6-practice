ba0d97efd510479de8bd30e1badbe84e5fcc7387
Fuyad <fuyad56@gmail.com>
2022-11-08 17:05:15 +0600

    Today's first commit

---
+++ Inheritance.js
@@ -0,0 +1,15 @@
+class Parent {
+    console(){
+        this.fatherName = "Schweznegger"
+    }
+}
+class Child{
+    constructor(name){
+        this.name = name;
+    }
+}
+
+const baby = new Child("Arnold");
+const baby2 = new Child("Tom")
+console.log(baby);
+console.log(baby2);
---
+++ class.js
@@ -0,0 +1,11 @@
+class student {
+    constructor(sId, sName){
+        this.id = sId;
+        this.name = sName;
+        this.name = "Kolimunnesa School"
+    }
+}
+const student1 = new student(12, "Shuvo");
+const student2 = new student(22, "Mahia");
+const student3 = new student(29, "Bappi")
+console.log(student1, student2, student3);
